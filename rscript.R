28f9f0ed52fd05eb001d301331cd1b84b14dc1bf
Sam Breaux <sam.fbreaux@gmail.com>
2019-09-11 13:27:45 -0700

    x

--- rscript.R
+++ rscript.R
@@ -3,11 +3,15 @@ library(tidyverse)
 library(plyr)
 library(reshape2)
 library(compare)
-
+library(rowr)
+library(HardyWeinberg)
 
 setwd("mydna")
 getwd()
 
+
+
+snpdat<-read.csv("mydna snps - Sheet1.csv")
 mydata<-read.csv("C:/Users/SamBr/Documents/pgx/mygenotypes.csv")%>%
   select(-X)%>%
   rename(c('gene' = 'GENE', 'genotype' = 'GENOTYPE'))
@@ -22,11 +26,11 @@ data <- rbindlist(temp)%>%
 
 
 
-write_csv (data, "C:/Users/SamBr/Documents/pgx/all_mydna.csv")
+write_csv (data, "C:/Users/SamBr/Documents/pgx/results/tidy_mydna.csv")
 
 xx<-split(data, data$GENE, drop = F)
 #names(xx)
-#CYP2D6<-xx$CYP2D6
+#CYP2D6<-xx$CYP2D6   
 
 
 
@@ -37,6 +41,9 @@ xx<-split(data, data$GENE, drop = F)
 # merge(CYP2D6, by = 'GENOTYPE')
 
 
+
+
+###calculate HWB equlibrium for CYP2D6 genes
 genotype_freqs<-function(df){
   v<-table(df$GENOTYPE)%>%
   as.data.frame()%>%
@@ -59,7 +66,7 @@ frequencies<-merge(mydata, data_Freq, by = c("GENE","GENOTYPE"))
 
 frequencies<- format(frequencies, digits = 2)
 
-write_csv (frequencies, "C:/Users/SamBr/Documents/pgx/myfreq.csv")
+write_csv (frequencies, "C:/Users/SamBr/Documents/pgx/results/myDNA_freq.csv")
 
 kdata<-read.csv("C:/Users/SamBr/Documents/pgx/allkailos3.csv")
 
@@ -71,6 +78,8 @@ m<-melt(kd, id.vars = "ID", measure.vars = 2:39)%>%
   mutate_all(~replace(., . == "", NA)) %>%
   na.omit()%>%
   rename(c('variable' = 'GENE', 'value'='kailos_call'))
+
+write.csv(m, "C:/Users/SamBr/Documents/pgx/results/tidykailos.csv")
 
 kg<-split(m, m$ID, drop =FALSE)
 
@@ -84,6 +93,8 @@ lol<-table(m$kailos_call)%>%
   unique()%>%
   format(digits = 2)
 
+write.csv(diffrentsamples, "C:/Users/SamBr/Documents/pgx/results/kailos_freq.csv")
+
 
 ff<-split(m, m$GENE, drop =FALSE)
 md<-mydata$GENE
@@ -95,7 +106,8 @@ k_call_only<-function(x){filter(x, grepl(paste(toMatch, collapse="|"), kailos_ca
 
 toMatch<-c('rs9923231', 'rs762551', 'rs4149056','CYP2D6', 'CYP2C19', 'CYP3A4', 'CYP3A5', 'CYP2C9')
 k_calls_only<-filter(m, grepl(paste(toMatch, collapse="|"), kailos_call))
-my_v_k
+
+
 kailosco_freq <-table(k_calls_only$kailos_call)%>%
   as.data.frame()%>%
   rename(c('Var1' = 'kailos_call'))%>%
@@ -106,18 +118,72 @@ kailosco_freq <-table(k_calls_only$kailos_call)%>%
   unique()%>%
   format(digits = 2) 
 
+
 mydata_geno<-merge(mydata, data, by = c("GENE","GENOTYPE"))
 my_v_k<-merge( k_calls_only,mydata_geno, by = c("GENE","ID"), all = T) 
 
-my_v_k$kailos_call<-na.omit(my_v_k$kailos_call.x, drop= T)
 
 
-#merge in other kailos_call column 
-mmm<-my_v_k[!is.na(my_v_k$kailos_call.x),]
+#t60-163 and t60-169 failed mydna genotyping
+# think L65-294 and L65-295 were swapped L65-293 =L65-296?
+mmm<-my_v_k[!is.na(my_v_k$kailos_call.x),]%>%
+  select( -GENOTYPE, -population.level, -phenotype, -'PREDICTED FUNCTION')%>%
+  rename(c("kailos_call.x" = 'kailos', 'kailos_call.y' = "myDNA"))
+
+  
+mmm$compare <-ifelse(mmm$kailos == mmm$myDNA,  "x",ifelse(mmm$kailos != mmm$myDNA, "different", "a"))
+diffrentsamples<-filter(mmm, compare == "different")
+
+write.csv(diffrentsamples, "C:/Users/SamBr/Documents/pgx/results/different_genotypes.csv")
+write.csv(mmm, "C:/Users/SamBr/Documents/pgx/results/my_vs_kaliosgenotypes.csv")
+
+l294 <- filter(mmm, ID == "L65-294")
+l295<- filter(mmm, ID == "L65-295")
+
+problemchildren<- cbind(l294,l295)
 
 
 
 mykailos<-merge(frequencies, kailosco_freq , by = "kailos_call", all = T)
 mykailos<-join(frequencies, lol, by = "kailos_call", match = "all")
+write.csv(mykailos, "C:/Users/SamBr/Documents/pgx/results/my_vs_kailos_freq_pop.csv")
 
-POP<-unique(lol)
+myunique<-unique(data$ID)
+kailosunique <-unique(m$ID)
+nnn<-cbind.fill(myunique, kailosunique, fill ="na")
+
+
+drugs<-read.csv("C:/Users/SamBr/Documents/pgx/drug-gene-md.csv")%>%
+  select(-'CPIC.Publications..PMID.')%>%
+  rename(c('Gene' = 'GENE'))
+
+mydrugs<-table(data$`PREDICTED FUNCTION`)%>%
+  as.data.frame()%>%
+  rename(c('Var1' = 'PREDICTED FUNCTION'))%>%
+  mutate(per_fun_freq = Freq/150*100)%>%
+  rename(c('Freq'= 'function_freq'))%>%
+  merge(data, by = 'PREDICTED FUNCTION')%>%
+  select(-ID, -GENOTYPE)%>%
+  unique()
+  
+function_freq_drug<-merge(mydrugs, drugs, by ="GENE")
+write.csv(function_freq_drug, "C:/Users/SamBr/Documents/pgx/results/myFUNCTION_drug_freq.csv")
+
+pheno_data<-mydata_geno%>%
+  select(phenotype, 'PREDICTED FUNCTION')
+
+
+
+fun_pheno<-merge(function_freq_drug, pheno_data, by = 'PREDICTED FUNCTION' )%>%
+  unique()
+fun_pheno<-fun_pheno %>% mutate_if(is.factor, as.character)%>%
+  mutate_if(is.character, str_trim)
+
+cast_pheno<-dcast(fun_pheno, Drug+GENE~phenotype, fun.aggregate = sum, value.var = "function_freq")
+
+
+
+#remove/replace warfin columns
+#calc hardy winberg
+#compare gene to RT data
+
